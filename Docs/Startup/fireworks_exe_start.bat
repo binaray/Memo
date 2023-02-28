323789fc768ef2d315a1eac8da269f549c6d2141
binaray <raychengcx@live.com>
2023-02-28 12:25:16 +0800

    Update fireworks_exe_start.bat

--- Docs/Startup/fireworks_exe_start.bat
+++ Docs/Startup/fireworks_exe_start.bat
@@ -1,1 +1,2 @@
-start /max C:\Users\Admin\Desktop\Firework_win\Fireworks.exe
+:: timeout 5  ::Sleep 5 seconds
+start /max C:\Users\Admin\Desktop\Firework_win\Fireworks.exe  ::Start application maximised; /min for opposite
